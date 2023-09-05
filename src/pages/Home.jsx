d40f59a5cb94c3c475ad95112c7e507f1857857d
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-04 23:05:15 -0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,14 @@ export default function Home() {
         <blockquote>
           My name is Mark Soro. I am an aspiring software developer based in
           Alexandria, Virginia, USA. I am passionate about creating practical,
-          user-friendly applications using JavaScript and Python.
+          user-friendly applications using JavaScript and Python.{" "}
         </blockquote>
-        <p></p>
       </section>
       <section>
-        <img src="" alt="" />
+        {/* <img
+          src="https://www.headshotpro.com/_nuxt/img/33.dc7f7bd.jpg"
+          alt=""
+        /> */}
       </section>
     </div>
   );
