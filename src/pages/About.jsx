48bfe7b85fb36b698a5901e0895999cfef200bb5
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-12 21:37:39 -0400

    update about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,38 @@ export default function About() {
     <div className="About">
       <h1>About</h1>
       <p>
-        Currently, I am in the process of completing a four-month, full-time
-        software development bootcamp.
+        I will soon complete Per Scholas four-month full time Software
+        Engineering Track as one important step of my pursuit of a career in
+        software development.
       </p>
+      <p>
+        Before beginning the tuition-free course which I was selected for, I
+        self studied and created projects of interest to me to prepare myself.
+      </p>
+      <p>
+        Working full-time in another field allowed me to save enough for
+        personal expenses to be able to take time off to complete the program.
+      </p>
+      <p>
+        I was priviledge to become a United States citizen in 2023, after
+        arriving from the Philippines with a little more than some provisions
+        and a point of contact for my exchange program.
+      </p>
+      <p>
+        As a first generation immigrant, I am grateful for every opportunity
+        this country has given me. I like to think that my strengths are my
+        perseverance, optimism and determination.
+      </p>
+      <p>
+        I am fluent in English and Tagalog and live in the DC area with my
+        partner.
+      </p>
+      <article>
+        <figure>
+          <img src="src/assets/images/z_profilePics/me002.jpg" alt="" />
+          <figcaption>In my U.S. citizenship oath taking ceremony</figcaption>
+        </figure>
+      </article>
     </div>
   );
 }
