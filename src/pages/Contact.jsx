56699048349b307220c8b191dc177457eab5f614
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-19 09:05:56 -0400

    added contact info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,13 @@ export default function Contact() {
   return (
     <div className="Contact">
       <h1>Get in touch</h1>
+      <div className="Contact-Options">
+        <ul>
+          <li>Email</li>
+          <li>LinkedIn</li>
+          <li>GitHub</li>
+        </ul>
+      </div>
     </div>
   );
 }
