86e1a1740e31eb8451cee807e1c166f42a40ffa5
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-20 15:59:01 -0400

    added animation for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,11 +3,11 @@ export default function Contact() {
     <div className="Contact">
       <h1>Get in touch</h1>
       <div className="Contact-Options">
-        <ul>
-          <li>Email</li>
-          <li>LinkedIn</li>
-          <li>GitHub</li>
-        </ul>
+        <div>Email</div>
+
+        <div>LinkedIn</div>
+
+        <div>GitHub</div>
       </div>
     </div>
   );
