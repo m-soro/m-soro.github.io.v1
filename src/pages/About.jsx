b2f829d5e1cc8619278705c57ed6377ccc8eb9f0
Mark Soro <napoleon_soro@rocketmail.com>
2024-03-09 20:22:13 -0500

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,20 +5,20 @@ export default function About() {
       <p>
         Hey there, welcome to my side of the web! I recently completed a
         rigorous four-month Software Engineering Certificate Program at Per
-        Scholas—a pivotal step in my software development journey. 🎓
+        Scholas a major step in my software development journey.
       </p>
       <p>
-        Before diving into this awesome tuition-free program, I spent
+        Before focusing into this awesome tuition-free program, I spent
         considerable time self-studying and working on personal projects that
-        align with my interests, gearing up for the challenges ahead. 💻📚
+        are interest interest of mine, gearing up for the challenges ahead.
       </p>
       <p>
         Balancing a full-time role in a different field, I managed to set aside
         enough resources to cover my expenses, allowing me to fully immerse
-        myself in the program. 💼💡
+        myself in the program.
       </p>
       <p>
-        In 2023, I finally got my United States citizenship—huge moment
+        In 2023, I finally got my United States citizenship — huge moment
         considering I first landed from the Philippines with just one contact
         person. Being a first-gen American, I'm truly thankful for the
         opportunities this country has opened up for me. 🇵🇭🇺🇸
@@ -35,7 +35,7 @@ export default function About() {
         <figure>
           <img
             src="https://github.com/m-soro/m-soro.github.io/blob/main/src/assets/images/z_profilePics/me002.jpg?raw=true"
-            alt=""
+            alt="Mark Soro, in his US Citizenhip ceremony"
           />
           <figcaption>In my U.S. citizenship oath taking ceremony</figcaption>
         </figure>
