35e1e5fd52fb6facfa35321fc13479b2f07cc5cb
Mark Soro <napoleon_soro@rocketmail.com>
2024-12-19 13:15:44 -0500

    updated impact bullets

--- src/assets/myFiles/ImpactData.jsx
+++ src/assets/myFiles/ImpactData.jsx
@@ -1,7 +1,7 @@
 export const ImpactData = [
   {
     id: 1,
-    title: "Efficient Case Auto-Closure System",
+    title: "Efficient Case Auto-Closure",
     image:
       "https://images.unsplash.com/photo-1553775282-20af80779df7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content:
