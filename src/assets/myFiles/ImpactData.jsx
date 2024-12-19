bde4e1b3e3538d693d474b8f72811e8f726db041
Mark Soro <napoleon_soro@rocketmail.com>
2024-12-19 13:12:37 -0500

    added impact bullets

--- src/assets/myFiles/ImpactData.jsx
+++ src/assets/myFiles/ImpactData.jsx
@@ -1,6 +1,22 @@
 export const ImpactData = [
   {
     id: 1,
+    title: "Efficient Case Auto-Closure System",
+    image:
+      "https://images.unsplash.com/photo-1553775282-20af80779df7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content:
+      "Worked on auto-closure system for various cases. Implemented educational modals that reduced unneccessary case creation boosting service efficiency.",
+  },
+  {
+    id: 2,
+    title: "Cross-Platform UX Enhancement",
+    image:
+      "https://plus.unsplash.com/premium_photo-1683865775631-3283bfaf6508?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3VzdG9tZXJ8ZW58MHx8MHx8fDA%3D",
+    content:
+      "Implemented layout improvements in customer facing site and internal platform, resulting in more intuitive user experience and reduced user frustration.",
+  },
+  {
+    id: 3,
     title: "Smart Request Management",
     image:
       "https://plus.unsplash.com/premium_photo-1681487526392-6183829c5f80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGFja2FnZXxlbnwwfHwwfHx8MA%3D%3D",
@@ -8,7 +24,7 @@ export const ImpactData = [
       "Implemented intelligent package tracking rules, reducing unnecessary requests and enhancing service efficiency.",
   },
   {
-    id: 2,
+    id: 4,
     title: "Re-worked Customer Feedback",
     image:
       "https://plus.unsplash.com/premium_photo-1682310144714-cb77b1e6d64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZmVlZGJhY2t8ZW58MHx8MHx8fDA%3D",
@@ -16,7 +32,7 @@ export const ImpactData = [
       "Re-worked the personnel compliment system, enabling effective recognition and maintaining service transparency.",
   },
   {
-    id: 3,
+    id: 5,
     title: "Enhanced Data Accuracy",
     image:
       "https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fG9ubGluZSUyMGZvcm18ZW58MHx8MHx8fDA%3D",
