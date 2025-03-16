f290025649d8d1c9dbe921381171c5eadc75f3f2
Mark Soro <napoleon_soro@rocketmail.com>
2025-03-16 13:54:34 -0400

    added impact bullet, updated resume

--- src/assets/myFiles/ImpactData.jsx
+++ src/assets/myFiles/ImpactData.jsx
@@ -1,15 +1,24 @@
 export const ImpactData = [
   {
     id: 1,
-    title: "Efficient Case Auto-Closure",
+    title: "Self-Service Case Creation API Integration",
     image:
-      "https://images.unsplash.com/photo-1553775282-20af80779df7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1586769852836-bc069f19e1b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXBpfGVufDB8fDB8fHww",
     content:
-      "Enhanced auto-closure logic with Apex and LWC components, reducing unnecessary case creation and improving efficiency.",
-    footer: "Federal Government Project, 2024",
+      "Developed a RESTful API integration enabling customers to create inquries through interactive voice response system, eliminating the need for agent interaction. Implemented comprehensive error handling and field validation across multiple Apex classes.",
+    footer: "Federal Government Project, 2025",
   },
   {
     id: 2,
+    title: "Service Request Process Enhancement",
+    image:
+      "https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fG9ubGluZSUyMGZvcm18ZW58MHx8MHx8fDA%3D",
+    content:
+      "Restricted outdated service request types by modifying profiles, permissions, and record types, while implementing sharing rules to ensure existing cases remained accessible for resolution.",
+    footer: "Federal Government Project, 2025",
+  },
+  {
+    id: 3,
     title: "Congressional Case Handling",
     image:
       "https://plus.unsplash.com/premium_photo-1683865775631-3283bfaf6508?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3VzdG9tZXJ8ZW58MHx8MHx8fDA%3D",
@@ -18,7 +27,16 @@ export const ImpactData = [
     footer: "Federal Government Project, 2025",
   },
   {
-    id: 3,
+    id: 4,
+    title: "Efficient Case Auto-Closure",
+    image:
+      "https://images.unsplash.com/photo-1553775282-20af80779df7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content:
+      "Enhanced auto-closure logic with Apex and LWC components, reducing unnecessary case creation and improving efficiency.",
+    footer: "Federal Government Project, 2024",
+  },
+  {
+    id: 5,
     title: "Smart Request Management",
     image:
       "https://plus.unsplash.com/premium_photo-1681487526392-6183829c5f80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGFja2FnZXxlbnwwfHwwfHx8MA%3D%3D",
@@ -27,21 +45,12 @@ export const ImpactData = [
     footer: "Federal Government Project, 2024",
   },
   {
-    id: 4,
+    id: 6,
     title: "Re-worked Customer Feedback",
     image:
       "https://plus.unsplash.com/premium_photo-1682310144714-cb77b1e6d64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZmVlZGJhY2t8ZW58MHx8MHx8fDA%3D",
     content:
       "Re-worked the personnel compliment system, enabling effective recognition and maintaining service transparency.",
     footer: "Federal Government Project, 2024",
-  },
-  {
-    id: 5,
-    title: "Service Request Process Enhancement",
-    image:
-      "https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fG9ubGluZSUyMGZvcm18ZW58MHx8MHx8fDA%3D",
-    content:
-      "Restricted outdated service request types by modifying profiles, permissions, and record types, while implementing sharing rules to ensure existing cases remained accessible for resolution.",
-    footer: "Federal Government Project, 2025",
   },
 ];
